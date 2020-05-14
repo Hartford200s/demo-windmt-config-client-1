c6d9875b3933dff51cf6d2a3ff21174ebcfc100a
Tai <test>
2020-05-14 16:21:12 +0800

    first commit

---
+++ src/main/java/com/example/demo/DemoWindmtConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWindmtConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWindmtConfigClientApplication.class, args);
+	}
+
+}
